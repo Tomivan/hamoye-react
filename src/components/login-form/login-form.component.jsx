7b6e3263c82d5f9cb8182476b1e5e580abd47294
Tomivan <gracejunkie20@gmail.com>
2023-05-03 23:07:31 +0100

    added pages and components

---
+++ src/components/login-form/login-form.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./login-form.css";
+
+const LoginForm = () => {
+    return(
+        <div className="login-form"></div>
+    )
+}
+
+export default LoginForm;
---
+++ src/components/table/table.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./table.css"
+
+const Table = () => {
+    return(
+        <div className="table"></div>
+    )
+}
+
+export default Table;
